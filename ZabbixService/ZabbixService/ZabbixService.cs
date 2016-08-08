6b31b7f85a71602346866d270548aed2957e4ec8
Apetrich <apetrichsheva@gmail.com>
2016-08-08 15:17:06 +0600

    1

---
+++ ZabbixService/ZabbixService/ZabbixService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ZabbixService
+{
+    public partial class ZabbixService : ServiceBase
+    {
+        private Int32 Interval;
+        private DateTime LastRequestTime;
+        public ZabbixService()
+        {
+            this.Interval = 15;
+            this.LastRequestTime = DateTime.Now;
+
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //var interval = ConfigurationManager.AppSettings["interval"];
+            //var lastRequestTime = ConfigurationManager.AppSettings["last-request-time"];
+
+
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
